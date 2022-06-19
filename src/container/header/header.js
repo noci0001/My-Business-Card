2822afc0b844f92f9c11614fc20ec1dafebe2cea
Sam <noci0001@hz.nl>
2022-06-19 22:01:41 +0200

    done?

--- src/container/header/header.js
+++
@@ -1,5 +0,0 @@
-const proPic = document.getElementById('proPic')
-
-proPic.addEventListener('dbclick', function(){
-    window.open("https://i.kym-cdn.com/entries/icons/original/000/039/393/cover2.jpg");
-})
